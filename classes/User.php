76a56ac9a043a3adb052b66b53d81d7bac5f482a
Theagns <markus.agnsgaard@hotmail.com>
2023-08-30 17:30:03 +0200

    user

---
+++ classes/User.php
@@ -0,0 +1,30 @@
+<?php
+
+class User {
+    private $_db,
+            $_data,
+            $_sessionName,
+            $_cookieName,
+            $isLoggedIn;
+
+    public function __construct($user = null) {
+        $this->_db = DB::getInstance();
+        $this->_sessionName = Config::get('sessions/session_name');
+        $this->_cookieName = Config::get('remember/cookie_name');
+
+        if(!$user) {
+            if(Session::exists($this->_sessionName)) {
+                $user = Session::get($this->_sessionName);
+
+                if($this->find($user)) {
+                    $this->isLoggedIn = true;
+                } else {
+                    //Logout
+                }
+            }
+        } else {
+            $this->find($user);
+        }
+    }
+
+    
