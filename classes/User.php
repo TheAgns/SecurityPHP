0e61cf9b50567064d208faf808e411bea0b1fd9a
Theagns <markus.agnsgaard@hotmail.com>
2023-08-30 17:30:29 +0200

    public function hasPermission() {

--- classes/User.php
+++ classes/User.php
@@ -27,4 +27,67 @@ class User {
         }
     }
 
-    
+    public function create($fields = array()) {
+        if(!$this->_db->insert('users', $fields)) {
+            throw new Exception('Sorry, there was a problem creating your account;');
+        }
+    }
+
+    public function update($fields = array(), $id = null) {
+
+        if(!$id && $this->isLoggedIn()) {
+            $id = $this->data()->id;
+        }
+
+        if(!$this->_db->update('users', $id, $fields)) {
+            throw new Exception('There was a problem updating');
+        }
+    }
+
+    public function find($user = null) {
+        if($user) {
+            $field = (is_numeric($user)) ? 'id' : 'username';
+            $data = $this->_db->get('users', array($field, '=', $user));
+
+            if($data->count()) {
+                $this->_data = $data->first();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function login($username = null, $password = null, $remember = false) {
+        if(!$username && !$password && $this->exists()) {
+            Session::put($this->_sessionName, $this->data()->id);
+        } else {
+            $user = $this->find($username);
+
+            if ($user) {
+                if(Hash::isValidPassword($password, $this->data()->password)) {
+                    Session::put($this->_sessionName, $this->data()->id);
+
+                    if ($remember) {
+                        $hash = Hash::unique();
+                        $hashCheck = $this->_db->get('users_session', array('user_id', '=', $this->data()->id));
+
+                        if (!$hashCheck->count()) {
+                            $this->_db->insert('users_session', array(
+                                'user_id' => $this->data()->id,
+                                'hash' => $hash
+                            ));
+                        } else {
+                            $hash = $hashCheck->first()->hash;
+                        }
+
+                        Cookie::put($this->_cookieName, $hash, Config::get('remember/cookie_expiry'));
+                    }
+
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+   
