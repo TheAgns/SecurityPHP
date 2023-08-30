50ee61047626ec102a295a140e566a48c4328f41
Theagns <markus.agnsgaard@hotmail.com>
2023-08-30 17:25:20 +0200

    Project structure and login and logout

---
+++ functions/sanitize.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'core/init.php';
+
+function escape($string) {
+    return htmlentities($string, ENT_QUOTES, 'UTF-8');
+}
+//Denne funktion bruger vi ti at sanitize user inputtet før det bliver vist på web pagen, som nedsætter riskoen for eventuelle security flaws som fx. cross-site scripting
