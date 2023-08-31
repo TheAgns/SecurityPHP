ec362ecf0576d23eeb10fc1ad2a80d99c6eb6050
Theagns <markus.agnsgaard@hotmail.com>
2023-08-31 13:07:57 +0200

    navbar added

---
+++ navbar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        .navbar {
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px;
+        }
+        
+        .navbar a {
+            color: white;
+            text-decoration: none;
+            margin-right: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="navbar">
+        <a href="index.php">Back to Index</a>
+        <a href="logout.php">Logout</a>
+        <a href="products.php">Cart 🛒🛒 </a>        
+    </div>
+</body>
+</html>
