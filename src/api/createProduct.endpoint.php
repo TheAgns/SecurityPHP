5b724aa2c6cd8b5ef6a0ab87d96cd8f864ac6b4c
Theagns <markus.agnsgaard@hotmail.com>
2023-09-07 11:33:32 +0200

    create product

--- src/api/createProduct.endpoint.php
+++ src/api/createProduct.endpoint.php
@@ -2,14 +2,22 @@
 
 require_once("src/core/init.php");
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $productName = $_POST['name'];
-    $price = $_POST['price'];
-    $img_url = $_POST['img_url'];
+// Vi skal tjekke om brugeren er logget ind, og om brugeren har de nødvendige tilladelser til at kunne oprette et produkt 
 
-    $productController = new ProductController();
-    $productController->createProduct($productName, $price, $img_url);
+try {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $productName = $_POST['name'];
+        $price = floatval($_POST['price']);
+        $img_url = $_POST['img_url'];
+        //Her skal vi tjekke igen om brugeren er logget ind, og om brugeren har de nødvendige tilladelser til at kunne oprette et produkt
 
-    header("location: /securityphp/products");
+        $productController = new ProductController();
+        $productController->createProduct($productName, $price, $img_url);
+
+        header("location: /securityphp/products");
+        exit();
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
 }
 ?>
