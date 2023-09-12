e80949f133e0ff40b95023afa026139ef458be4c
Theagns <markus.agnsgaard@hotmail.com>
2023-09-12 11:04:11 +0200

    logger

--- logger.php
+++ logger.php
@@ -1,12 +1,22 @@
 <?php 
+// function logger($log){
+//   if(!file_exists('log.txt')){
+//       file_put_contents('log.txt', '');
+//   }
+//   $ip = $_SERVER['REMOTE_ADDR'];
+//   $time = date('Y-m-d H:i:s', time());
+//   $content = file_get_contents('log.txt');
+//   $content .= "[$time] $ip $log\n";
+//   file_put_contents('log.txt', $content);
+// }
+?>
+<?php
 function logger($log){
-  if(!file_exists('log.txt')){
-      file_put_contents('log.txt', '');
-  }
+  $logFileName = date('Y-m-d') . '.txt';
   $ip = $_SERVER['REMOTE_ADDR'];
   $time = date('Y-m-d H:i:s', time());
-  $content = file_get_contents('log.txt');
+  $content = file_get_contents($logFileName);
   $content .= "[$time] $ip $log\n";
-  file_put_contents('log.txt', $content);
+  file_put_contents($logFileName, $content);
 }
 ?>
