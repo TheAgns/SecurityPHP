b70bf5a7b55bd060cae9d39236dd059cc294d50b
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2023-09-12 12:43:47 +0200

    adminView and userView on product-list.php

--- src/classes/ProductView.php
+++ src/classes/ProductView.php
@@ -9,24 +9,39 @@ class ProductView extends Product
 
     public function showAllProducts($data)
     {
-
-        foreach ($data as $product) {
-            $html = '<div class="product-card">';
-            $html .=
-                '<img src="' . $product['img_url'] .
-                '" alt="' . $product['name'] .
-                '" class="product-image"><br>' .
-                '<h2>' . $product['name'] . '</h2>' .
-                '<p>Price: ' . number_format($product['price'], 2) . ',- ddk</p>' .
-                '<form method="post">' .
-                '<input type="hidden" name="product_id" value="' . $product['id'] . '">' .
-                '<button type="submit" class="buy-button">Buy</button>' .
-                '</form>' .
-                '</div>';
-
-            echo $html;
+        $validate = new Validate();
+        if ($validate->hasPermission("admin")) {
+            foreach ($data as $product) {
+                $html = '<div class="product-card">';
+                $html .=
+                    '<img src="' . $product['img_url'] .
+                    '" alt="' . $product['name'] .
+                    '" class="product-image"><br>' .
+                    '<h2>' . $product['name'] . '</h2>' .
+                    '<p>Price: ' . number_format($product['price'], 2) . ',- ddk</p>' .
+                    '<form action="" method="post">' .
+                    '<input type="hidden" name="product_id" value="' . $product['id'] . '">' .
+                    '<button type="submit" class="buy-button">Edit</button>' .
+                    '</form>' .
+                    '</div>';
+                echo $html;
+            }
+        } else if ($validate->hasPermission("user")) {
+            foreach ($data as $product) {
+                $html = '<div class="product-card">';
+                $html .=
+                    '<img src="' . $product['img_url'] .
+                    '" alt="' . $product['name'] .
+                    '" class="product-image"><br>' .
+                    '<h2>' . $product['name'] . '</h2>' .
+                    '<p>Price: ' . number_format($product['price'], 2) . ',- ddk</p>' .
+                    '<form action="" method="post">' .
+                    '<input type="hidden" name="product_id" value="' . $product['id'] . '">' .
+                    '<button type="submit" class="buy-button">Add to Cart</button>' .
+                    '</form>' .
+                    '</div>';
+                echo $html;
+            }
         }
-
-
     }
 }
