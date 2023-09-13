7b5dbc5ee15df484845fb0fdd4d0de922261a475
Thomas <cph-ta181@cphbusiness.dk>
2023-09-13 14:04:06 +0200

    Users can only access their own orders

--- order-detail.php
+++ order-detail.php
@@ -1,7 +1,6 @@
 <?php
 require_once('src/core/init.php');
 
-
 $order_id  = escape($_GET["id"]);
 $orderController = new OrderController();
 $order = $orderController->getOrder($order_id);
@@ -9,6 +8,16 @@ $order = $orderController->getOrder($order_id);
 $orderLineController = new OrderLineController();
 $orderLine = $orderLineController->getOrderOrderLine($order_id);
 
-$orderView = new OrderView();
-$orderView->showOrderDetails($order, $orderLine);
+if(Session::exists(Config::Get("sessions/userid"))){
+    if($order[0]['user_id'] == Session::get(Config::get("sessions/userid"))){
+        $orderView = new OrderView();
+        $orderView->showOrderDetails($order, $orderLine);
+    }
+    else{
+        echo 'NO access';
+    }
+}
+else{
+    echo 'no session';
+}
 ?>
