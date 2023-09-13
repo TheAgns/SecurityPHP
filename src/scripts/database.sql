3680e2aedbdb4d5dd9c26eabfd60687c0edeb27e
Theagns <markus.agnsgaard@hotmail.com>
2023-09-13 12:13:21 +0200

    update to script

--- src/scripts/database.sql
+++ src/scripts/database.sql
@@ -63,36 +63,9 @@ CREATE TABLE Basket (
   FOREIGN KEY (orderLine_id) REFERENCES Orderline(id)
 );
 
-
--- New SQL Dump
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Sep 13, 2023 at 12:10 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `securityphp`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `groups`
---
 
 CREATE TABLE `groups` (
   `id` int(11) NOT NULL,
@@ -100,19 +73,10 @@ CREATE TABLE `groups` (
   `permissions` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `groups`
---
 
 INSERT INTO `groups` (`id`, `name`, `permissions`) VALUES
 (1, 'Adminstrator', '{\"admin\":1}'),
 (2, 'User', '{\"user\":1}');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `products`
---
 
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
@@ -121,19 +85,9 @@ CREATE TABLE `products` (
   `img_url` varchar(355) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `products`
---
-
 INSERT INTO `products` (`id`, `name`, `price`, `img_url`) VALUES
 (1, 'Macbook m1', 9000, ' https://digitalassets.sallinggroup.com/image/upload/e_trim:2/w_287,h_287,c_pad,dpr_2.0,ar_1,q_40,f_auto,b_white,e_sharpen:80/ca862a28208d4fe6b35499988f0a967d'),
 (13, 'test', 2222, 'www.google.com');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -143,58 +97,22 @@ CREATE TABLE `users` (
   `group` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `group`) VALUES
-(1, 'Markus', '$2y$10$cuU9dqsg8YYjXGYaW2k9g.JjPrSGY91nU1DCXSkMiUUK3NCXdVbYe', 'markus@gmail.com', 2);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `groups`
---
 ALTER TABLE `groups`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `products`
---
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `groups`
---
 ALTER TABLE `groups`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `products`
---
 ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
